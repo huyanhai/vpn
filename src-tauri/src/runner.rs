3e039cc8e8571ea3f10482c30b03dbd68d89414d
Huyanhai <810839700@qq.com>
2023-06-13 15:41:27 +0800

    fix: win文件路径修改

--- src-tauri/src/runner.rs
+++ src-tauri/src/runner.rs
@@ -12,13 +12,22 @@ pub mod Runner {
 
     impl Running {
         pub fn new(handle: tauri::AppHandle) -> Running {
-            let resource_path = handle
-                .path_resolver()
-                .resolve_resource("../resources/SSHProxy-1.5.jar")
-                .expect("failed to resolve resource");
-            Running {
-                file_path: format!("{}", resource_path.to_string_lossy()),
+            let mut file_path = String::from("");
+            if cfg!(target_os = "windows") {
+                let resource_path = handle
+                    .path_resolver()
+                    .resolve_resource("..\\resources\\SSHProxy-1.5.jar")
+                    .expect("failed to resolve resource");
+                file_path = format!("{}", resource_path.to_string_lossy())
+            } else {
+                let resource_path = handle
+                    .path_resolver()
+                    .resolve_resource("../resources/SSHProxy-1.5.jar")
+                    .expect("failed to resolve resource");
+                file_path = format!("{}", resource_path.to_string_lossy())
             }
+
+            Running { file_path }
         }
         pub fn run(self) {
             thread::spawn(|| {
